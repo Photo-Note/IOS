1d0070d8ef76d3146173f95d4f87a7f11652abd4
Brandi <blubrandi0@gmail.com>
2020-01-02 12:08:50 -0700

    Swipe Gestures and Nav Controller
    Built tutorial scenes and created VC files.  Hiding nav bar on tut pages.  Troubleshooting:  last tutorial page does not swipe back.

---
+++ PhotoNote/PhotoNote/TutorialStepTwoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TutorialStepTwoViewController.swift
+//  PhotoNote
+//
+//  Created by Brandi Bailey on 1/2/20.
+//  Copyright © 2020 brian vilchez. All rights reserved.
+//
+
+import UIKit
+
+class TutorialStepTwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
