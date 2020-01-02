3922613ee2c027158b07e6fb90272ad58eba869f
Alex Shillingford <alex.shillingford@icloud.com>
2020-01-02 12:10:21 -0700

    Core Data Models and initializers set up

---
+++ PhotoNote/PhotoNote/Models/Model+Convenience.swift
@@ -0,0 +1,30 @@
+//
+//  Model+Convenience.swift
+//  PhotoNote
+//
+//  Created by Alex Shillingford on 1/2/20.
+//  Copyright © 2020 brian vilchez. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Photo {
+    convenience init(name: String, photo: String, partner: String?, note: String?, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        self.init(context: context)
+        
+        self.name = name
+        self.photo = photo
+        self.partner = partner
+        self.note = note
+    }
+    
+//    convenience init?(photoRepresentation: PhotoRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+//
+//        guard let partner = photoRepresentation.partner,
+//            let note = photoRepresentation.note else { return }
+//
+//        self.init(name: photoRepresentation.name, photo: photoRepresentation.photo, partner: partner, note: note, context: context)
+//    }
+}
