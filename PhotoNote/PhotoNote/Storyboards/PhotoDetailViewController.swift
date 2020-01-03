a1b1d514dabe7a4428b922bcd60c86a567ac369e
Alex Shillingford <alex.shillingford@icloud.com>
2020-01-02 20:13:18 -0700

    Create PhotoDetailViewController

---
+++ PhotoNote/PhotoNote/Storyboards/PhotoDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PhotoDetailViewController.swift
+//  PhotoNote
+//
+//  Created by Alex Shillingford on 1/2/20.
+//  Copyright © 2020 brian vilchez. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+    
+    @IBOutlet weak var imageViewPhotoDetail: UIImageView!
+    @IBOutlet weak var dropOrFlipSegmentControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
